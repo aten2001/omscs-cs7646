20df10c6d00f299e630e27b689e16bf69ae2a117
Joo <joo@stashaway.com>
2017-04-10 10:19:11 +0800

    matplotlib practice

--- mc3p3_reports/mc3p3.py
+++ mc3p3_reports/mc3p3.py
@@ -141,31 +141,94 @@ def show_difference_scaling():
 
 
 def SMA(df, n):
-    SMA = pd.Series(pd.rolling_mean(df['Adj Close'], n), name = 'SMA_' + str(n))
+    # pd.Series.rolling(window=10, center=False).mean()
+    SMA = pd.Series(pd.rolling_mean(df['Adj Close'], n), name='SMA')
     df = df.join(SMA)
     return df
 
-#Exponential Moving Average
+
+# Exponential Moving Average
 def EMA(df, n):
-    EMA = pd.Series(pd.ewma(df['Adj Close'], span = n, min_periods = n - 1), name = 'EMA_' + str(n))
+    EMA = pd.Series(pd.ewma(df['Adj Close'], span=n, min_periods=n - 1), name='EMA')
     df = df.join(EMA)
     return df
 
-#Bollinger Bands
+
+# Bollinger Bands
 def BBANDS(df, n):
-    MA = pd.Series(pd.rolling_mean(df['Close'], n))
-    MSD = pd.Series(pd.rolling_std(df['Close'], n))
+    MA = pd.Series(pd.rolling_mean(df['Adj Close'], n))
+    MSD = pd.Series(pd.rolling_std(df['Adj Close'], n))
     b1 = 4 * MSD / MA
-    B1 = pd.Series(b1, name = 'BollingerB_' + str(n))
+    B1 = pd.Series(b1, name='BollingerB_' + str(n))
     df = df.join(B1)
     b2 = (df['Close'] - MA + 2 * MSD) / (4 * MSD)
-    B2 = pd.Series(b2, name = 'Bollinger%b_' + str(n))
+    B2 = pd.Series(b2, name='Bollinger%b_' + str(n))
     df = df.join(B2)
     return df
+
 
 def show_prices_sma(prices, window):
     price_with_sma = pd.rolling_mean(prices, window)
 
+
+def show_sma():
+    unique_symbols = ['AAPL']
+    dates = pd.date_range('2010-01-01', '2012-12-31')
+    prices_df = get_data(symbols=unique_symbols,
+                         dates=dates,
+                         addSPY=True,
+                         colname='Adj Close')
+
+    prices_df = prices_df.rename(columns={'AAPL': 'Adj Close'})
+    SMA_AAPL = SMA(prices_df, 20)
+    SMA_AAPL = SMA_AAPL.dropna()
+    SMA = SMA_AAPL['SMA']
+
+    plt.figure(figsize=(9, 5))
+    plt.plot(prices_df['Adj Close'], lw=1, label='AAPL Prices')
+    plt.plot(SMA, 'g', lw=1, label='20 day SMA (green)')
+    plt.legend(loc=2, prop={'size': 11})
+    plt.grid(True)
+    plt.setp(plt.gca().get_xticklabels(), rotation=30)
+    plt.show()
+
+
+def get_rolling_mean(values, window):
+    return pd.rolling_mean(values, window=window)
+
+
+def get_rolling_std(values, window):
+    return pd.rolling_std(values, window=window)
+
+
+def get_bollinger_bands(rm, rstd):
+    upper_band = rm + rstd * 2
+    lower_band = rm - rstd * 2
+    return upper_band, lower_band
+
+
+def show_bollinger_band(prices_df, symbol):
+    rm = get_rolling_mean(prices_df[symbol], window=20)
+    rstd = get_rolling_std(prices_df[symbol], window=20)
+    upper_band, lower_band = get_bollinger_bands(rm, rstd)
+
+    # Plt raw values, rolling mean and Bollinger Bands
+    ax = prices_df[symbol].plot(title="Bollinger Bands", label=symbol)
+    rm.plot(label='Rolling Mean', ax=ax)
+    upper_band.plot(label='upper band', ax=ax)
+    lower_band.plot(label='lower band', ax=ax)
+
+    lower_indicator = lower_band - prices_df[symbol]
+    upper_indicator = prices_df[symbol] - upper_band
+
+    lower_indicator.plot(label='lower bound  - price', ax=ax)
+    upper_indicator.plot(label='price - upper bound', ax=ax)
+
+    # Add axis labels and legend
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    ax.legend(loc='upper left')
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -181,13 +244,25 @@ if __name__ == "__main__":
     # show_histogram()
 
     unique_symbols = ['AAPL']
-    start_dt = '2010-01-01'
-    last_dt = '2010-12-31'
-    prices_df = get_data(unique_symbols, pd.date_range(start_dt, last_dt), False, 'Adj Close')
-    prices_df.plot()
-    plt.show()
-    # plt.show()
+    start_dt = '2010-01-04'
+    last_dt = '2011-04-26'
+    prices_df = get_data(symbols=unique_symbols,
+                         dates=pd.date_range(start_dt, last_dt),
+                         addSPY=True,
+                         colname='Adj Close')
+
+    # prices_df = prices_df.rename(columns={'AAPL': 'Adj Close'})
+    # SMA_AAPL = SMA(prices_df, 20)
+    # SMA_AAPL = SMA_AAPL.dropna()
+    # SMA = SMA_AAPL['SMA']
+
+    # plt.figure(figsize=(9, 5))
+    # plt.plot(prices_df['Adj Close'], lw=1, label='AAPL Prices')
+    # plt.plot(SMA, 'g', lw=1, label='20 day SMA (green)')
+    # plt.legend(loc=2, prop={'size': 11})
+    # plt.grid(True)
+    # plt.setp(plt.gca().get_xticklabels(), rotation=30)
     # plt.show()
 
-    #show_difference_scaling()
-    #plt.show()
+    show_bollinger_band(prices_df, 'AAPL')
+    plt.show()
